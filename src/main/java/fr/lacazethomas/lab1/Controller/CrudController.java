2b08bca062c1075fc4cc2e476a4ca33e9b9c86b7
lacazethomas <contact@thomaslacaze.fr>
2021-11-17 19:14:47 +0100

    add circuit breaker

--- src/main/java/fr/lacazethomas/lab1/Controller/CrudController.java
+++ src/main/java/fr/lacazethomas/lab1/Controller/CrudController.java
@@ -4,16 +4,18 @@ import fr.lacazethomas.lab1.controller.dto.BaseDTO;
 import fr.lacazethomas.lab1.service.CrudService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.annotations.ApiOperation;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public abstract class CrudController<T extends BaseDTO> {
@@ -23,7 +25,6 @@ public abstract class CrudController<T extends BaseDTO> {
     @Value("${backendA:http://localhost:8081}")
     private String backendA;
 
-    @Autowired
     private RestTemplate restTemplate;
 
     @Bean
@@ -33,14 +34,21 @@ public abstract class CrudController<T extends BaseDTO> {
 
     public CrudController(CrudService<T> crudService) {
         this.service = crudService;
+        this.restTemplate = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setRedirectStrategy(new LaxRedirectStrategy())
+                .build();
+        factory.setHttpClient(httpClient);
+        restTemplate.setRequestFactory(factory);
     }
 
     @GetMapping("")
     @ApiOperation(value = "List all")
     @CircuitBreaker(name = "MovieService", fallbackMethod = "getAllFallback")
     public ResponseEntity<String> getAll() {
-        String response = restTemplate.getForObject(backendA+"/movies", String.class);
-        return new ResponseEntity<String>(response, HttpStatus.OK);
+        ResponseEntity<String> resp = restTemplate.exchange(backendA + "/movies",HttpMethod.GET, null,String.class);
+        return new ResponseEntity<>((String) resp.getBody(), resp.getStatusCode());
     }
 
     public ResponseEntity<List<T>> getAllFallback(Exception e) {
@@ -51,8 +59,8 @@ public abstract class CrudController<T extends BaseDTO> {
     @ApiOperation(value = "Get by Id")
     @CircuitBreaker(name = "MovieService", fallbackMethod = "getByIdFallback")
     public ResponseEntity<String> getById(@PathVariable Long id) {
-        String response = restTemplate.getForObject(backendA+"/movies/" + id, String.class);
-        return new ResponseEntity<String>(response, HttpStatus.OK);
+        ResponseEntity<String> resp = restTemplate.exchange(backendA + "/movies/"+id, HttpMethod.GET, null,String.class);
+        return new ResponseEntity<>((String) resp.getBody(), resp.getStatusCode());
     }
 
     public ResponseEntity<T> getByIdFallback(@PathVariable Long id, Exception e) {
@@ -65,13 +73,26 @@ public abstract class CrudController<T extends BaseDTO> {
 
     @PostMapping("")
     @ApiOperation(value = "Create a new one")
-    public ResponseEntity<T> save(@RequestBody T body) {
+    @CircuitBreaker(name = "MovieService", fallbackMethod = "saveFallback")
+    public ResponseEntity<String> save(@RequestBody T body) {
+        ResponseEntity<String> resp = restTemplate.postForEntity(backendA+"/movies/", body, String.class);
+        return new ResponseEntity<>((String) resp.getBody(), resp.getStatusCode());
+    }
+
+    public ResponseEntity<T> saveFallback(@RequestBody T body, Exception e) {
         return new ResponseEntity<>(service.save(body), HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
     @ApiOperation(value = "Delete by Id")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    @CircuitBreaker(name = "MovieService", fallbackMethod = "deleteFallback")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        ResponseEntity<String> resp = restTemplate.exchange(backendA + "/movies/"+id,HttpMethod.DELETE, null,String.class);
+
+        return new ResponseEntity<>((String) resp.getBody(), resp.getStatusCode());
+    }
+
+    public ResponseEntity<?> deleteFallback(@PathVariable Long id, Exception e) {
         Optional<T> optional = service.findById(id);
         optional.ifPresent(n -> service.delete(id));
 
@@ -81,7 +102,13 @@ public abstract class CrudController<T extends BaseDTO> {
 
     @PutMapping("{id}")
     @ApiOperation(value = "Update by Id")
+    @CircuitBreaker(name = "MovieService", fallbackMethod = "updateFallback")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody T body) {
+        ResponseEntity<String> resp = restTemplate.exchange(backendA + "/movies/"+id,HttpMethod.PUT, null,String.class);
+        return new ResponseEntity<>((String) resp.getBody(), resp.getStatusCode());
+    }
+
+    public ResponseEntity<?> updateFallback(@PathVariable Long id, @RequestBody T body, Exception e) {
         Optional<T> optional = service.findById(id);
         optional.ifPresent(n -> service.update(id, body));
 
