2fe12db3630a789f70d561757d6102612e8e3ad5
lacazethomas <contact@thomaslacaze.fr>
2021-11-24 17:13:27 +0100

    Fix async test

--- src/test/java/fr/lacazethomas/lab1/MovieControllerIntegrationTest.java
+++ src/test/java/fr/lacazethomas/lab1/MovieControllerIntegrationTest.java
@@ -13,20 +13,20 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-
 import java.time.LocalDate;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -73,20 +73,21 @@ public class MovieControllerIntegrationTest {
     @Test
     public void getMovies() throws Exception {
         // Given
-
-
         var m1 = new Movie("je suis un title", LocalDate.of(2020, 1, 8), "je suis un plot");
         repository.save(m1);
         var m2 = new Movie("test2", LocalDate.of(2020, 1, 8), "test2plot");
         repository.save(m2);
 
         // When
-        mvc.perform(
+        MvcResult mvcResult = mvc.perform(
                         get("/movies")
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
-
-                // Then
+                .andExpect(request().asyncStarted())
+                .andDo(MockMvcResultHandlers.log())
+                .andReturn();
+        // Then
+        mvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].title", is("je suis un title")))
@@ -97,6 +98,7 @@ public class MovieControllerIntegrationTest {
                 .andExpect(jsonPath("$[1].plot", is("test2plot")))
                 .andExpect(jsonPath("$[1].releaseDate", is("2020-01-08")));
     }
+
 
     @Test
     public void deleteMovie() throws Exception {
@@ -134,19 +136,24 @@ public class MovieControllerIntegrationTest {
                                 .content(objectMapper.writeValueAsString(editDto)))
                 .andExpect(status().isOk());
 
-        // Then
-        mvc.perform(
+
+        // When
+        MvcResult mvcResult = mvc.perform(
                         get("/movies")
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
-
-                // Then
+                .andExpect(request().asyncStarted())
+                .andDo(MockMvcResultHandlers.log())
+                .andReturn();
+        // Then
+        mvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].title", is("je suis un title 2")))
                 .andExpect(jsonPath("$[0].id", notNullValue()))
                 .andExpect(jsonPath("$[0].releaseDate", is("2021-02-09")))
                 .andExpect(jsonPath("$[0].plot", is("je suis un plot 2")));
+
     }
 
 }
